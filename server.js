9c146cfa7929018bc51e67c1f1b1ea793f5df5f3
Amir <amho.pubg@gmail.com>
2025-09-12 12:34:46 +0330

    main

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+// ارسال کردن ایمیل
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+
+mongoose.connect('mongodb://localhost:27017/MyWebSite').
+    then(() => { console.log('connect to mongodb') }).
+    catch(() => { console.log('cloud not connect to mongodb') })
+
+global.config = require('./config');
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+// Middleware Send Email
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+// Route برای ارسال ایمیل
+app.post('/send-email', async (req, res) => {
+    const { name, email, phone, subject, message } = req.body;
+
+    // پیکربندی nodemailer
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MYEMAIL,
+            pass: process.env.PASSWORDEMAIL
+        }
+    });
+
+    // تنظیمات ایمیل
+    let mailOptions = {
+        from: email,
+        to: process.env.MYEMAIL, // ایمیل مقصد (همان ایمیل شما)
+        subject: subject,
+        text: `
+            نام: ${name}
+            ایمیل: ${email}
+            تلفن: ${phone}
+            موضوع: ${subject}
+            پیام: ${message}
+        `
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('ایمیل با موفقیت ارسال شد');
+    } catch (error) {
+        console.error('Error sending email: ', error);
+        res.status(500).send('خطا در ارسال ایمیل');
+    }
+
+});
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.use('/', require('./routes/index'));
+
+
+app.listen(config.port, () => { console.log(`listen port ${config.port}`) })
